70af6bcebcaf2a9741794ef2545a4c6688f272a2
castaldoarcangelo88-source <castaldoarcangelo88@gmail.com>
2026-05-14 20:21:17 +0200

    Update CONFIG export format in config.js

--- modules/config.js
+++ modules/config.js
@@ -1,4 +1,3 @@
-// ⚠️ FILE SENSIBILE - NON CONDIVIDERE
 export const CONFIG = {
   SUPABASE_URL: "https://igmmekejpqbbctjupsqa.supabase.co",
   SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlnbW1la2VqcHFiYmN0anVwc3FhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2ODQ4MjgsImV4cCI6MjA5NDI2MDgyOH0.TYFey7KCDrWSikDEsDDIba7E65sD0EhS1CF-tsKRGJs",
